79b00e57b69591f5d0f9882f7bb807f602fd8fb6
ytyaru <ytyaru@outlook.jp>
2019-09-25 12:57:05 +0900

    フィードのみ取得するコードを作成した

--- src/get_news.py
+++ src/get_news.py
@@ -4,29 +4,51 @@
 # stdin  : 必須。RSS URL
 # 第1引数: 任意。SQLite3DBファイルパス（デフォルト＝カレントディレクトリ/news.db）
 # 例1: echo -e 'RSS1\nRSS2\nRSS3' | python3 get_news.py ./news.db
-# 例2: cat rss_list_source1.txt   | python3 get_news.py ./news.db
+# 例2: cat feeds_source1.txt   | python3 get_news.py ./news.db
 import sys
 import os
 import feedparser
 from mod import DateTimeString
 from mod import HtmlGetter
 from mod import HtmlContentExtractor
+from mod import FeedDb
 from mod import NewsDb
 from mod import NewsImagesDb
 
-rss_list = sys.stdin.readlines() # 標準入力がなければ永久に待機してしまう！
-if 0 == len(rss_list):
+feeds = sys.stdin.readlines() # 標準入力がなければ永久に待機してしまう！
+if 0 == len(feeds):
     raise Error(("stdinにRSSのURLを指定してください。コード例は以下。\n" + 
                 "echo 'RSS1' | python3 get_news.py" + 
                 "echo -e 'RSS1\nRSS2\nRSS3' | python3 get_news.py ./news.db\n" + 
-                "cat rss_list_source1.txt   | python3 get_news.py ./news.db\n" + 
+                "cat feeds_source1.txt   | python3 get_news.py ./news.db\n" + 
                 "第1引数にSQLite3DBファイル出力パスを指定できます。デフォルトは「./news.db」です。"))
     exit()
 db_dir_path = sys.argv[1] if (1 < len(sys.argv)) else os.path.join(os.getcwd(), 'news.db')
 
-for rss in rss_list:
+def has_def(obj, attr, default): 
+    return getattr(obj, attr) if hasattr(obj, attr) else default
+
+# フィードのみ取得
+feed_db_path = '/tmp/work/feeds.db'
+os.makedirs(os.path.dirname(feed_db_path), exist_ok=True)
+dtcnv = DateTimeString.DateTimeString()
+feed_db = FeedDb.FeedDb('/tmp/work/feeds.db')
+for feed in feeds:
+    entries = feedparser.parse(feed).entries
+    for entry in entries:
+        published = dtcnv.convert_utc((
+            has_def(entry, 'published', entry.updated))
+            ).strftime('%Y-%m-%dT%H:%M:%SZ')
+        url = entry.link
+        title = entry.title
+        summary = has_def(entry, 'summary', '')
+        feed_db.append_news(published,url,title,summary=summary)
+    feed_db.insert() # :memory:へ
+feed_db.marge() # 指定パスへ
+
+# 本文抽出
+for rss in feeds:
     entries = feedparser.parse(rss).entries
-    dtcnv = DateTimeString.DateTimeString()
     #extractor = HtmlContentExtractor.HtmlContentExtractor()
     extractor = HtmlContentExtractor.HtmlContentExtractor(option={"threshold":50})
     getter = HtmlGetter.HtmlGetter()
@@ -41,7 +63,6 @@ for rss in rss_list:
         url = entry.link
         title = entry.title
         if news_db.is_exists(published,url): continue
-    #    body = extractor.extract(get_html.get_html(url))
         body = extractor.extract(getter.get(url))
         news_db.append_news(published, url, title, body);
     #    break; # HTML取得を1件だけでやめる
---
+++ src/mod/FeedDb.py
@@ -0,0 +1,95 @@
+import sqlite3
+import os
+import operator
+# フィード内容をDBにする。本文取得が完了するまでの間だけ使う予定（本文取得が10分以上と遅すぎるため）
+# RAMディスクに配置する予定
+
+class FeedDb:
+    def __init__(self, path):
+        self.__schema_ram = 'r'
+        self.conn = sqlite3.connect(':memory:')
+        self.conn.cursor().execute("attach '"+path+"' as '"+self.__schema_ram+"';")
+#        self.conn = sqlite3.connect(path)
+#        self.conn.row_factory = sqlite3.Row
+        self.__create_table()
+        self.__create_table(schema_name=self.__schema_ram)
+        self.news = []
+    def __del__(self): self.conn.close()
+    # schema_name: main, temp, (attach-db-name)
+    def __schema_name(self, schema_name=None): return (
+        '' if schema_name is None or 0 == len(schema_name) else (schema_name 
+           if schema_name.endswith('.') else schema_name + '.'))
+    def __create_table(self, schema_name=''):
+        cur = self.conn.cursor()
+        print(schema_name)
+        print(self.__create_table_sql(schema_name=schema_name))
+        cur.executescript(self.__create_table_sql(schema_name=schema_name))
+    def __create_table_sql(self, schema_name=''):
+        return '''
+create table if not exists {schema_name}news(
+  id         integer primary key,
+  published  text not null,
+  url        text not null,
+  title      text not null,
+  summary    text not null default '',
+  UNIQUE(url,title) -- 記事の一意確認
+);
+create index if not exists {schema_name}idx_news on 
+  news(published desc, url, title, id desc);
+'''.format(schema_name=self.__schema_name(schema_name))
+    def __get_latest_sql(self, schema_name=''): return '''
+with 
+  latest(max_published) as (select max(published) max_published from {schema_name}news),
+  maximum(max_id) as (
+    select max(id) as max_id
+    from {schema_name}news,latest
+    where {schema_name}news.published=latest.max_published;
+  )
+select 
+  {schema_name}news.published, 
+  {schema_name}news.url, 
+  {schema_name}news.title
+from {schema_name}news,maximum
+where {schema_name}news.id=maximum.max_id;
+'''.format(schema_name=schema_name)
+    def __insert_sql(self, schema_name=''): 
+        return 'insert or ignore into {schema_name}news(published,url,title,summary) values(?,?,?,?)'.format(schema_name=self.__schema_name(schema_name))
+    def append_news(self, published, url, title, summary=''):
+        self.news.append((published, url, title, summary))
+    def insert(self):
+        if 0 == len(self.news): return
+        try:
+            self.news = sorted(self.news, key=operator.itemgetter(1)) # 第2キー: URL昇順
+            self.news = sorted(self.news, key=operator.itemgetter(0), reverse=True) # 第1キー: 公開日時降順
+            self.conn.cursor().executemany(self.__insert_sql(), self.news)
+            self.conn.commit()
+        except: # それ以外
+            import traceback
+            traceback.print_exc()
+            self.conn.rollback() # ロールバックする
+        finally: self.news.clear()
+    def __marge_sql(self):
+        return 'insert or ignore into {schema_ram}news(published,url,title,summary) select published,url,title,summary from {schema_mem}news;'.format(
+            schema_ram=self.__schema_name(self.__schema_ram),
+            schema_mem=''
+        )
+    # インメモリDBからRAMディスクDBへ
+    def marge(self):
+        try:
+            self.conn.cursor().execute(self.__marge_sql())
+            self.conn.commit()
+        except sqlite3.IntegrityError as err_sql_integ:
+            import traceback
+            import sys
+            msg = str(err_sql_integ.with_traceback(sys.exc_info()[2])).lower() # UNIQUE constraint failed: news.published, news.url
+            # DB既存と重複した時点で中断する（それまでの挿入データはそのままにしたいのでロールバックしない）
+            if ('UNIQUE'.lower() in msg and 'published' in msg and 'url' in msg): pass
+            # それ以外ならエラー表示＆ロールバックする
+            else: 
+                traceback.print_exc()
+                self.conn.rollback() 
+        except: # それ以外
+            import traceback
+            traceback.print_exc()
+            self.conn.rollback() # ロールバックする
+
